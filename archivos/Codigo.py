80eb987a4810e716192a2e1d714f95fe5f7877ec
Daniel Eduardo Rojas Garza <danymessi1000@gmail.com>
2020-05-23 21:15:06 -0500

    Agregue la opcion 1 y 2 corregidas del menu 1 del archivo Codigo.py

--- archivos/Codigo.py
+++ archivos/Codigo.py
@@ -14,12 +14,62 @@ if Menu==1:
     opcion=int(input("Que opcion elige: "))
 
     if opcion==1:
-        archivoA=open("./archivos/Empleados.txt",'a')
-        idEmpleado=(input("Ingrese su numero de Empleado: "))
-        nombre=(input("Ingrese su Nombre: "))
-        direccion=(input("Ingrese su Direccion: "))
-        archivoA.write(idEmpleado + "|"+ nombre + "|" + direccion)
-        archivoA.close()
+        def main():
+            archivoA=open("./archivos/Empleados.txt",'a')
+            idEmpleado=(input("Ingrese su numero de Empleado: "))
+            nombre=(input("Ingrese su Nombre: "))
+            direccion=(input("Ingrese su Direccion: "))
+            archivoA.write(idEmpleado + "|"+ nombre + "|" + direccion + "|" + "\n")
+            archivoA.close()
+        main()
+
+    elif opcion==2:
+        def main2():
+            borrar=(input("Que numero de IdEmpleado deseas borrar: "))
+            archivoB=open("./archivos/Empleados.txt",'r')
+            miarchivo = open("./archivos/Nuevo.txt",'x')
+            miarchivo.close()
+            miarchivo = open("./archivos/Nuevo.txt",'w')
+            
+            for renglon in archivoB:
+                datosempleado = renglon.split('|')
+                ide=datosempleado[0]
+                nom=datosempleado[1]
+                dire=datosempleado[2]
+                
+                if ide!=borrar:
+                    miarchivo.write(ide + "|"+ nom + "|" + dire + "|" + "\n")
+
+            archivoB.close()
+
+            from os import remove
+            remove("./archivos/Empleados.txt")
+
+            miarchivo= open("./archivos/Nuevo.txt","r")
+            archivodestino = open("./archivos/Empleados.txt","w")
+            archivodestino.write(miarchivo.read())
+            miarchivo.close()
+            archivodestino.close()
+
+            from os import remove
+            remove("./archivos/Nuevo.txt")
+        main2()
+
+
+    elif opcion==3:
+        pass
+
+
+
+
+
+
+        
+
+
+
+            
+
 
 
 
